08e01b469d9566023f7fe9fec98352bba96484d4
kungpaockn <kevingoenadibrata@gmail.com>
2021-11-18 14:50:09 -0800

    dotenv debug 2

--- src/Fetchers/index.js
+++ src/Fetchers/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
+dotenv.config();
 const serverDomain = process.env.SERVER_DOMAIN || "http://localhost:3001";
 console.log(`Using Server: ${serverDomain}`);
 
